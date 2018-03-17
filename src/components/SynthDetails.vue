b1fb04eee3ccdf9985d2c935e53ed7343ee016e9
lp1 <mail@lp1.eu>
2018-03-17 16:49:12 +0100

    Added filters

--- src/components/SynthDetails.vue
+++ src/components/SynthDetails.vue
@@ -1,5 +1,6 @@
 <template>
-  <div class="app-container synth-details" v-if="synth.type !== 'classic'">
+<div class="synth-details">
+  <div class="app-container synth-conf" v-if="synth.type !== 'classic'">
     <h3>{{ synth.name }}</h3>
     <p>Type: {{ synth.type }} synthetizer</p>
     <p>Amplitude: {{ synth.amplitude }} Hz</p>
@@ -7,9 +8,20 @@
     <p>Samples: {{ synth.sin.length }}</p>
     <p v-if="synth.type === 'function'">Function: {{ synth.function.toString() }}</p>
   </div>
-  <div class="app-container synth-details" v-else>
-    Select a custom synthetizer to display its details
+  <div class="app-container synth-filter" v-if="synth.filter">
+    <h3>Filter</h3>
+    <p>Type:
+    <select v-model="synth.filter.type" @input="synth.filter.updateType()">
+      <option v-for="(type, index) in synth.filter.types" :key="index">
+        {{ type }}
+      </option>
+    </select>      
+      </p>
+    <p>Frequency: <input type="number" v-model="synth.filter.frequency" @input="synth.filter.updateFrequency()"></p>
+    <p>Gain: <input type="number" v-model="synth.filter.gain" @input="synth.filter.updateGain()"></p>
+    <p>Q: <input type="number" v-model="synth.filter.q" @input="synth.filter.updateQ()"></p>
   </div>
+</div>
 </template>
 
 <script>
@@ -20,8 +32,9 @@ export default {
 </script>
 <style>
 .synth-details {
-  margin: 1vh;
   width: 40%;
+}
+.synth-conf{
   padding: 1vh
 }
 </style>
--- src/utils/Synthetizers.js
+++ src/utils/Synthetizers.js
@@ -1,28 +1,40 @@
-import { Synthetizer, CustomSynthetizer, FunctionSynthetizer } from '../lib/seaboard-api/js/MIDI'
+import { SubstractiveSynthetizer, CustomSubstractiveSynthetizer, FunctionSubstractiveSynthetizer } from '../lib/seaboard-api/js/Synthetizers'
+import { Filter } from '../lib/seaboard-api/js/Filters'
+
+const filters = {
+  lowpass: new Filter('lowpass', 1000, 25, 9),
+  highpass: new Filter('highpass', 1000, 25, 9),
+  bandpass: new Filter('bandpass', 1000, 25, 9),
+  lowshelf: new Filter('lowshelf', 1000, 25, 9),
+  highshelf: new Filter('highshelf', 1000, 25, 9),
+  peaking: new Filter('peaking', 1000, 25, 9),
+  notch: new Filter('notch', 1000, 25, 9),
+  allpass: new Filter('allpass', 1000, 25, 9)
+}
 
 const synthetizers = [
-  new Synthetizer('Synthetizer 1 (Classic Sine)'),
-  new Synthetizer('Synthetizer 2 (Classic Square)', {fadeTime: 0.3, type: 'square'}),
-  new Synthetizer('Synthetizer 3 (Classic Sawtooth)', {fadeTime: 0.3, type: 'sawtooth'}),
-  new Synthetizer('Synthetizer 4 (Classic Triangle)', {fadeTime: 0.3, type: 'triangle'}),
-  new CustomSynthetizer('Sine Climb', {fadeTime: 0.3}, [0, -3, -2, -1, 0, 1, 2, 3], [0, -3, 0, 0, 0, 0, 0, 0]),
-  new CustomSynthetizer('Organ', {fadeTime: 0.01}, [0, -3, 3, -3, 3], [0, 0, 0, 0, 0]),
-  new CustomSynthetizer('Faded Organ', {fadeTime: 1}, [0, -3, 3, -3, 3], [0, 0, 0, 0, 0]),
-  new CustomSynthetizer('Classic Synth', {fadeTime: 0.3}, [0, 0, 1], [0, 1, 0]),
-  new CustomSynthetizer('Magic Bells', {fadeTime: 0.9}, [0, 0, 0, 0, 0, -3], [0, 0, 0, 0, 0, 0]),
-  new CustomSynthetizer('Low Oscillator 1', {fadeTime: 0.3}, [1, 1, 0, 0], [1, 0, 0, 0]),
-  new CustomSynthetizer('Low Oscillator 2', {fadeTime: 0.3}, [6, 6, 0, 0], [0, 0, 0, 0], true),
-  new CustomSynthetizer('Horn', {fadeTime: 0.3}, [0, 0.4, 0.4, 1, 1, 1, 0.3, 0.7, 0.6, 0.5, 0.9, 0.8], new Int8Array(12)),
-  new FunctionSynthetizer('Creepy Exp Function Synth', {fadeTime: 0.3}, (x) => Math.pow(2 * Math.PI, x)),
-  new FunctionSynthetizer('Rhods Like', {fadeTime: 0.5}, (x) => -x + 5, false, 6),
-  new FunctionSynthetizer('DJames Bass 1', {fadeTime: 0.3}, (x) => Math.cos(x) * x * Math.PI, false, 50),
-  new CustomSynthetizer('Cheap Strings', {fadeTime: 0.7, gain: 0}, [1.5, 0.5, 1, -1.5, -1, -1.5, -1, -1.5, -1, 0, 0.5, 1.3, 1, 0, -0.5, -0.5, -0.5, -0.5, 0, 0.25, 0.5],
+  new SubstractiveSynthetizer('Synthetizer 1 (Classic Sine)', filters.lowpass),
+  new SubstractiveSynthetizer('Synthetizer 2 (Classic Square)', filters.lowpass, {fadeTime: 0.3, type: 'square'}),
+  new SubstractiveSynthetizer('Synthetizer 3 (Classic Sawtooth)', filters.lowpass, {fadeTime: 0.3, type: 'sawtooth'}),
+  new SubstractiveSynthetizer('Synthetizer 4 (Classic Triangle)', filters.lowpass, {fadeTime: 0.3, type: 'triangle'}),
+  new CustomSubstractiveSynthetizer('Sine Climb', filters.lowpass, {fadeTime: 0.3}, [0, -3, -2, -1, 0, 1, 2, 3], [0, -3, 0, 0, 0, 0, 0, 0]),
+  new CustomSubstractiveSynthetizer('Organ', filters.lowpass, {fadeTime: 0.01}, [0, -3, 3, -3, 3], [0, 0, 0, 0, 0]),
+  new CustomSubstractiveSynthetizer('Faded Organ', filters.lowpass, {fadeTime: 1}, [0, -3, 3, -3, 3], [0, 0, 0, 0, 0]),
+  new CustomSubstractiveSynthetizer('Classic Synth', filters.lowpass, {fadeTime: 0.3}, [0, 0, 1], [0, 1, 0]),
+  new CustomSubstractiveSynthetizer('Magic Bells', filters.lowpass, {fadeTime: 0.9}, [0, 0, 0, 0, 0, -3], [0, 0, 0, 0, 0, 0]),
+  new CustomSubstractiveSynthetizer('Low Oscillator 1', filters.lowpass, {fadeTime: 0.3}, [1, 1, 0, 0], [1, 0, 0, 0]),
+  new CustomSubstractiveSynthetizer('Low Oscillator 2', filters.lowpass, {fadeTime: 0.3}, [6, 6, 0, 0], [0, 0, 0, 0], true),
+  new CustomSubstractiveSynthetizer('Horn', filters.lowpass, {fadeTime: 0.3}, [0, 0.4, 0.4, 1, 1, 1, 0.3, 0.7, 0.6, 0.5, 0.9, 0.8], new Int8Array(12)),
+  new FunctionSubstractiveSynthetizer('Creepy Exp Function Synth', filters.lowpass, {fadeTime: 0.3}, (x) => Math.pow(2 * Math.PI, x)),
+  new FunctionSubstractiveSynthetizer('Rhods Like', filters.notch, {fadeTime: 0.5}, (x) => -x + 5, false, 6),
+  new FunctionSubstractiveSynthetizer('DJames Bass 1', filters.lowshelf, {fadeTime: 0.3}, (x) => Math.cos(x) * x * Math.PI, false, 50),
+  new CustomSubstractiveSynthetizer('Cheap Strings', filters.lowpass, {fadeTime: 0.7, gain: 0}, [1.5, 0.5, 1, -1.5, -1, -1.5, -1, -1.5, -1, 0, 0.5, 1.3, 1, 0, -0.5, -0.5, -0.5, -0.5, 0, 0.25, 0.5],
   new Int8Array(21), false),
-  new FunctionSynthetizer('Negative Pow Function', {fadeTime: 0.3}, (x) => Math.pow(x, 0.9), false, 20),
-  new CustomSynthetizer('Clean Space Synth', {fadeTime: 0.5}, [0, 1, 2, 3, 2, 0, 0, 0], [0, 1, 2, 3, 2, 0, 0, 0], false, 5),
-  new FunctionSynthetizer('Old School Synth', {fadeTime: 0.5}, (x) => x * Math.sin(x) + x * Math.tan(x), false, 100),
-  new FunctionSynthetizer('FlashBack', {fadeTime: 0.5}, (x) => Math.tan(x) + (Math.pow(x, 3) / 100), false, 13),
-  new FunctionSynthetizer('High Pitched Buzzer', {fadeTime: 0.5}, (x) => Math.tan(x) + (Math.pow(x, 1.2) / 10), false, 40)
+  new FunctionSubstractiveSynthetizer('Negative Pow Function', filters.lowpass, {fadeTime: 0.3}, (x) => Math.pow(x, 0.9), false, 20),
+  new CustomSubstractiveSynthetizer('Clean Space Synth', filters.lowpass, {fadeTime: 0.5}, [0, 1, 2, 3, 2, 0, 0, 0], [0, 1, 2, 3, 2, 0, 0, 0], false, 5),
+  new FunctionSubstractiveSynthetizer('Old School Synth', filters.lowpass, {fadeTime: 0.5}, (x) => x * Math.sin(x) + x * Math.tan(x), false, 100),
+  new FunctionSubstractiveSynthetizer('FlashBack', filters.lowpass, {fadeTime: 0.5}, (x) => Math.tan(x) + (Math.pow(x, 3) / 100), false, 13),
+  new FunctionSubstractiveSynthetizer('High Pitched Buzzer', filters.lowpass, {fadeTime: 0.5}, (x) => Math.tan(x) + (Math.pow(x, 1.2) / 10), false, 40)
 ]
 
 export default synthetizers
